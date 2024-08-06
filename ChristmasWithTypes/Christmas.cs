e0b641f5591d001e59ca1dab83a81ef260e08f8b
Darron Manning <darronmanning@yahoo.com>
2024-08-06 10:52:12 -0500

    finished main method

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -4,7 +4,7 @@ namespace ChristmasWithTypes
     public class Christmas
     {
         public string[] Presents;
-        public string Santa { get; set; }
+        public string Santa { get; set;  }
         public int? TreeHeight { get; set; } //Done Make the Height property nullable
 
         //Done Make the property, "Day", type enum
